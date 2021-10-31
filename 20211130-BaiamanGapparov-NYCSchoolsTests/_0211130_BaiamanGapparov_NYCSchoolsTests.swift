3a7c99c3fe717d459a6edbf496b3cf88713de515
Bayaman Gapparov <bayamangapparov@gmail.com>
2021-10-30 20:38:25 -0500

    Setup project to github.

---
+++ 20211130-BaiamanGapparov-NYCSchoolsTests/_0211130_BaiamanGapparov_NYCSchoolsTests.swift
@@ -0,0 +1,33 @@
+//
+//  _0211130_BaiamanGapparov_NYCSchoolsTests.swift
+//  20211130-BaiamanGapparov-NYCSchoolsTests
+//
+//  Created by Baiaman Gapparov on 10/30/21.
+//
+
+import XCTest
+@testable import _0211130_BaiamanGapparov_NYCSchools
+
+class _0211130_BaiamanGapparov_NYCSchoolsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
